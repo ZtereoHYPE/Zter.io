aa86cdc156e2dbc4426a9286f0301a2184f6f532
ZtereoHYPE <57519662+ZtereoHYPE@users.noreply.github.com>
2021-05-23 00:49:46 +0200

    change: better username interface

--- public/ui.js
+++ public/ui.js
@@ -1,13 +1,15 @@
 let username;
-document.querySelector('#submitButton').addEventListener('click', () => {
+document.querySelector('#submitButton').addEventListener('click', playGame)
+
+function playGame() {
     if (!document.querySelector('#usernameField').value) {
         alert('Please fill in the username field');
         return;
     }
     username = document.querySelector('#usernameField').value;
-    document.querySelector('#form').style.display = 'none';
+    document.querySelector('.container').style.display = 'none';
     let mainScript = document.createElement('script');
     mainScript.type = 'text/javascript';
     mainScript.src = 'main.js';
     document.getElementsByTagName('body')[0].appendChild(mainScript);
-})
+}

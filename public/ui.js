d8022809dce0a4cefd12041fe68d32e9b38fceb7
ZtereoHYPE <lorenzodegrandis@icloud.com>
2021-05-23 01:05:40 +0200

    change: add username character limit

--- public/ui.js
+++ public/ui.js
@@ -1,12 +1,16 @@
 let username;
-document.querySelector('#submitButton').addEventListener('click', playGame)
 
 function playGame() {
-    if (!document.querySelector('#usernameField').value) {
+    username = document.querySelector('#usernameField').value
+    if (!username) {
         alert('Please fill in the username field');
         return;
     }
-    username = document.querySelector('#usernameField').value;
+    if (username.length > 16) {
+        alert('Please enter a username between 1 and 16 characters.');
+        return;
+    }
+    // username = document.querySelector('#usernameField').value;
     document.querySelector('.container').style.display = 'none';
     let mainScript = document.createElement('script');
     mainScript.type = 'text/javascript';
